842b9444cb41f04bd8b0794b40de6521079f14c2
Luken <github@luken.cc>
2022-12-23 01:08:48 +0100

    :tada: init sveltekit project

---
+++ src/lib/utils/GridSearch.ts
@@ -0,0 +1,60 @@
+export type Path = Point[]
+export type Grid = string[][]
+export type Dictionary = string[]
+
+export interface Point {
+	x: number
+	y: number
+}
+
+export interface Result {
+	word: string
+	path: Path
+}
+
+export function findWordsDFS(grid: Grid, dictionary: Dictionary): Result[] {
+	const results: Result[] = []
+	const width = grid[0].length - 1
+	const height = grid.length - 1
+
+	function visitCell(x: number, y: number, path: Path, dict: Dictionary) {
+		if (x < 0 || x > width || y < 0 || y > height) {
+			return
+		}
+
+		path.push({ x, y })
+
+		const word = path.map((p) => grid[p.y][p.x]).join("")
+		dict = dict.filter((w) => w.startsWith(word))
+
+		if (dict.length === 0) {
+			return
+		}
+
+		if (dict.includes(word)) {
+			results.push({ word, path })
+		}
+
+		for (let i = -2; ++i < 2;) {
+			for (let j = -2; ++j < 2;) {
+				if (i === 0 && j === 0) {
+					continue
+				}
+
+				const next = { x: x + j, y: y + i }
+
+				if (path.findIndex((p) => p.x === next.x && p.y === next.y) === -1) {
+					visitCell(next.x, next.y, [ ...path ], dict)
+				}
+			}
+		}
+	}
+
+	for (let y = 0; y < grid.length; ++y) {
+		for (let x = 0; x < grid[y].length; ++x) {
+			visitCell(x, y, [], dictionary)
+		}
+	}
+
+	return results
+}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import SpellCast from "$lib/components/SpellCast.svelte"
+</script>
+
+<svelte:head>
+	<title>SpellCaster</title>
+</svelte:head>
+
+<div class="container">
+	<SpellCast />
+</div>
+
+<style>
+	@font-face {
+		font-family: Satoshi;
+		src: url(fonts/Satoshi-Regular.otf);
+		font-weight: 400;
+	}
+
+	@font-face {
+		font-family: Satoshi;
+		src: url(fonts/Satoshi-Bold.otf);
+		font-weight: 700;
+	}
+
+	@font-face {
+		font-family: Satoshi;
+		src: url(fonts/Satoshi-Black.otf);
+		font-weight: 900;
+	}
+
+	:global(body) {
+		margin: 0;
+		background: #000b19;
+
+		font-family: Satoshi, serif;
+	}
+
+	.container {
+		height: 100vh;
+		overflow: hidden;
+
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
